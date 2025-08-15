90cb460d7b189902f4f77583f999e7b6e086f7b0
joseLuís <joseluis@andamira.net>
2025-08-15 11:11:58 +0200

    update docs

--- build_docs_unpublished.sh
+++ build_docs_unpublished.sh
@@ -12,22 +12,24 @@
 
 VERSION="0.25.0-wip" # UNPUBLISHED VERSION
 
-WORK_DIR="$HOME/dev/rust/libera/devela"
-GIT_DIR="$HOME/tmp/andamira.github.io/"
+source .env
+# LIB_DIR=""
+# GIT_DIR=""
 DEV_DIR="$GIT_DIR/devela/"
 TMP_DIR="$GIT_DIR/devela/tmp"
 OUT_DIR="$DEV_DIR/$VERSION"
 OUT_SYM="$DEV_DIR/unpublished"
 
-CMD="CARGO_TARGET_DIR=\"$TMP_DIR\" RUSTFLAGS=\"--cfg nightly -Ctarget-cpu=native\" RUSTDOCFLAGS=\"--generate-link-to-definition --html-in-header ./config/rustdoc-header.html --cfg nightly -Z unstable-options\" cargo +nightly doc -F _docsrs --no-deps"
+CMD="CARGO_TARGET_DIR=\"$TMP_DIR\" RUSTFLAGS=\"-Ctarget-cpu=native --cfg nightly\" RUSTDOCFLAGS=\"-Ctarget-cpu=native --cfg nightly -Z unstable-options --generate-link-to-definition --html-in-header ./config/rustdoc-header.html\" cargo +nightly doc --no-deps -F _docsrs"
 
 
 # compile docs
 echo "Building the unpublished version of devela docs. . ."
-cd "$WORK_DIR"
+cd "$LIB_DIR"
 echo $CMD
 eval $CMD
 
+cd "$GIT_DIR"
 # delete previous build
 rm -r "$OUT_DIR"
 # create `latest` symlink
